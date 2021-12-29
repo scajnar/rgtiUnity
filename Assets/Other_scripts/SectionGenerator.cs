654db124c061b82e32fbc8e28e9dcd645c3ec126
Vasja Lev Kirn <47994956+artemis358@users.noreply.github.com>
2021-12-29 17:51:43 +0300

    added one section

--- Assets/Other_scripts/SectionGenerator.cs
+++ Assets/Other_scripts/SectionGenerator.cs
@@ -8,9 +8,13 @@ public class SectionGenerator : MonoBehaviour
     public int zPos = 40; // Z pozicija kam se postavi prvi generirani section
     public bool creatingSection = false;
     public int secNum;
+    public int stevilo_sectionov = 1; //ena manj kot jih dejansko je
 
     IEnumerator GenerateSection(){
-        secNum = 0; //ko je vec kot 1 section -> Random.Range(0, stevilo_sectionov);
+        secNum = 1; //ko je vec kot 1 section -> Random.Range(0, stevilo_sectionov);
+
+        //secNum = Random.Range(0, stevilo_sectionov);
+
         Instantiate(section[secNum], new Vector3(0,0,zPos), Quaternion.identity);
         zPos += 40; //+Z pozicija kam se postavi vsaki naslednji generirani section
         yield return new WaitForSeconds(2);
